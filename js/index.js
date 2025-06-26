ce32c8a1cc21ba2b07bb1c456a735228a21b4dfb
Aditi22112004 <ayushisri5566@gmail.com>
2025-06-26 12:33:35 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+console.log("hello  node");
+var  a = "name";
+console.log (typeof a);
+a = 10;
+console.log (typeof a);
+c= 10;
+console.log (c);
+var c ;
---
+++ react/react taliwind/src/App.jsx
@@ -0,0 +1,19 @@
+
+
+const App = () => {
+  return (
+    <>
+    <div className="w-3/4 h-screen border-2 border-b-black m-auto">
+        Main div
+        <div className="w-[400px] h-[400px] flex flex-col items-center justify-endS border-2 border-t-black m-auto">
+          <div className="w-[40px] h-[40px] border-2 border-b-black">1</div>
+          <div className="w-[40px] h-[40px] border-b-black border-2">2</div>
+          <div className="w-[40px] h-[40px] border-b-black border-2">3</div>
+          <div className="w-[40px] h-[40px] border-b-black border-2">4</div>
+          </div> 
+          </div>
+      </>
+  )
+}; 
+
+export default App;
